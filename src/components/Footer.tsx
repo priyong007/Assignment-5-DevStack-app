dc613a9450ed7bd4e65681478b3e93ad1f0ef101
priyong007 <priyong.chakma@gmail.com>
2026-09-13 14:37:03 +0600

    footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
         
         <div>
 
-  <div className="max-w-[1340px] mx-auto my-20 px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-16">
+  <div className="max-w-[1340px] mx-auto my-22 px-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 lg:gap-16">
     
     {/* About */}
     <div className="sm:col-span-2 lg:col-span-1">
